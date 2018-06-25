a4022620c7bba890bf3e6938d8a24314078e69d7
Elijah Smith <elijahs2106@gmail.com>
2018-06-25 13:54:23 -0400

    Added comments to index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('button');
 
 function changeHeading(e) {
-    e.preventDefault();
+    e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
+    // Select heading and text input and change the heading to the value of the text input.
     const heading = document.querySelector('.changingHeading');
     const textInput = document.querySelector('input');
     const newHeading = textInput.value;

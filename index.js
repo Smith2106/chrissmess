2a854911402c3ae87f8d5f5f568c89b52c5495b2
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 11:51:32 -0400

    Change changeHeading function to addMovies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 
-function changeHeading(e) {
+function addMovies(e) {
     e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
     // Append movie entered in text input field to unordered list below the form.
     const flicksList = document.querySelector('#flicks');
@@ -10,7 +10,7 @@ function changeHeading(e) {
     const newListItem = document.createElement('li');
     newListItem.textContent = newFlick;
     flicksList.appendChild(newListItem);
-    
+
     e.target.reset();
 }
 

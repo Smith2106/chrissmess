0d41899bee7627e5a80b117ed55e4a3d96e672c8
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 11:49:45 -0400

    Added Day 2 Homework to README

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Chrissmess
 
+## Day 2 Homework
+
+* Give the funtion `changeHeading` a more accurate name.
+* Add a second field to the form.
+* Display the value of that field in the list.
+
+## Bonus Credit
+* Display the second field in a separate HTML element than the flick name. For example:
+```html
+<li>
+    <span class="flickName">Jurassic World</span>
+    <span class="year">2015</span>
+</li>
+```
+* See if you can then style it differently.
+
+# Super Mega Bonus Credit
+
+* Use more than one funciton (Break `changeHeading` into multiple functions).
+
 ## Day 1 Homework
 
 * Make the button change the text of the heading (the `<h1>`).
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const form = document.querySelector('form');
 
 function changeHeading(e) {
     e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
-    // Select heading and text input and change the heading to the value of the text input.
+    // Append movie entered in text input field to unordered list below the form.
     const flicksList = document.querySelector('#flicks');
     const textInput = e.target.movie;
     const newFlick = textInput.value;
+
     const newListItem = document.createElement('li');
     newListItem.textContent = newFlick;
     flicksList.appendChild(newListItem);
+    
     e.target.reset();
 }
 

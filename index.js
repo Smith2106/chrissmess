71a8a2d6e8f9ce15b8f26c592ee6f699ebd4c8f1
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 11:33:14 -0400

    Refactor appending innerHTML to appending child to movie list

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function changeHeading(e) {
     const flicksList = document.querySelector('#flicks');
     const textInput = e.target.movie;
     const newFlick = textInput.value;
-    flicksList.innerHTML += `<li>${newFlick}</li>`;
+    const newListItem = document.createElement('li');
+    newListItem.textContent = newFlick;
+    flicksList.appendChild(newListItem);
     e.target.reset();
 }
 

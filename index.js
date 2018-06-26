cbf4d3382cfd633fa5719ab2608f7fe2739a96d7
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 11:19:26 -0400

    Refactor to add movies to an unordered list

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const form = document.querySelector('form');
 function changeHeading(e) {
     e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
     // Select heading and text input and change the heading to the value of the text input.
-    const flicksDiv = document.querySelector('#flicks');
+    const flicksList = document.querySelector('#flicks');
     const textInput = e.target.movie;
     const newFlick = textInput.value;
-    flicksDiv.innerHTML += `<p>${newFlick}</p>`;
+    flicksList.innerHTML += `<li>${newFlick}</li>`;
     e.target.reset();
 }
 

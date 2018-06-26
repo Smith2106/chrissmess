49a4ac34ae7cf1bbe32020f1d099a1b326d12add
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 10:34:18 -0400

    Change form to listen for submit and change heading accordingly

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const button = document.querySelector('button');
+const form = document.querySelector('form');
 
 function changeHeading(e) {
     e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
     // Select heading and text input and change the heading to the value of the text input.
     const heading = document.querySelector('.changingHeading');
-    const textInput = document.querySelector('input');
+    const textInput = e.target.answer;
     const newHeading = textInput.value;
     heading.textContent = newHeading;
 }
 
-button.addEventListener('click', changeHeading);
+form.addEventListener('submit', changeHeading);

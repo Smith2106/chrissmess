62cfd85048c6022f24d00be69ce2750c630e8a6e
Elijah Smith <elijahs2106@gmail.com>
2018-06-26 12:00:47 -0400

    Change to display second input form value in list

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ function addMovies(e) {
     e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
     // Append movie entered in text input field to unordered list below the form.
     const flicksList = document.querySelector('#flicks');
-    const textInput = e.target.movie;
-    const newFlick = textInput.value;
+    const movie = e.target.movie.value;
+    const year = e.target.year.value;
 
-    const newListItem = document.createElement('li');
-    newListItem.textContent = newFlick;
-    flicksList.appendChild(newListItem);
+    const item = document.createElement('li');
+
+    item.textContent = `${movie}, (${year})`;
+    flicksList.appendChild(item);
 
     e.target.reset();
 }
 
-form.addEventListener('submit', changeHeading);
+form.addEventListener('submit', addMovies);

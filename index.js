430f398d5c2021b949a9cea4c155242ba7c875fc
Elijah Smith <elijahs2106@gmail.com>
2018-06-27 14:49:35 -0400

    Add delete button to each movie list element

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ class App {
 
     handleSubmit(e) {
         e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
-        
+        // Get flicks list and form.
         const flicksList = document.querySelector('#flicks');
         const flick = {};
         const form = e.target;
-    
+        // Set the properties of the flick object and add flicks to movies array
         this.getItemProps(flick, form);
         this.allMovies.push(flick);
-
+        // Render the properties in the item and render the item in the flicks list on the page
         const item = this.renderItem(flick);
         flicksList.appendChild(item);
     
+        // Reset the form and reset focus to first field.
         e.target.reset();
         e.target.movie.focus();
         console.log(this.allMovies);
+    }
+
+    handleDelete() {
+
     }
     
     getItemProps(flick, form) {
@@ -42,8 +47,10 @@ class App {
     }
     
     renderItem(flick) {
+        // Create item list element
         const item = document.createElement('li');
         item.classList.add('flick');
+        // Render the properties in the list element that aren't colors.
         const properties = Object.keys(flick);
         properties.forEach(property => {
             const span = this.renderProperty(property, flick[property]);
@@ -51,10 +58,15 @@ class App {
         });
     
         this.setColors(flick.backColor, flick.textColor, item);
+        // Create delete button and have it handle deletion of elements
+        const button = document.createElement('button');
+        button.textContent = 'Delete';
+        button.classList.add('delete-btn');
+        button.addEventListener('click', () => this.handleDelete);
+        item.appendChild(button);
     
         return item;
     }
-    
     
     setColors(backColor, textColor, item) {
         item.style.backgroundColor = backColor;

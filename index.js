eb2db97dc7f2757734b3db3314941264b73e0d3b
Elijah Smith <elijahs2106@gmail.com>
2018-06-27 11:53:42 -0400

    Refactor to avoid repetition in rendering text and refactor to object

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
-const form = document.querySelector('form');
+class App {
+    constructor() {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.handleSubmit(e);
+        });
+    }
 
-const addMovies = function(e) {
-    e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
-    // Append movie entered in text input field to unordered list below the form.
-    const flicksList = document.querySelector('#flicks');
-    const itemObj = {};
-    const form = e.target;
-
-    getItemProps(itemObj, form);
-
-    const item = createItem(itemObj);
+    handleSubmit(e) {
+        e.preventDefault(); // Prevent the form from submitting and automatically reloading the page.
+        
+        const flicksList = document.querySelector('#flicks');
+        const flick = {};
+        const form = e.target;
     
-    flicksList.appendChild(item);
-
-    e.target.reset();
-    e.target.movie.focus();
+        this.getItemProps(flick, form);
+        const item = this.renderItem(flick);
+        flicksList.appendChild(item);
+    
+        e.target.reset();
+        e.target.movie.focus();
+    }
+    
+    getItemProps(flick, form) {
+        flick.movie = form.movie.value;
+        flick.year = form.year.value;
+        flick.backColor = form.backColor.value;
+        flick.textColor = form.textColor.value;
+        flick.rating = form.rating.value;
+    }
+    
+    renderProperty(name, value) {
+        const span = document.createElement('span');
+        span.textContent = value;
+        span.classList.add(name);
+        return span;
+    }
+    
+    renderItem(flick) {
+        const item = document.createElement('li');
+        item.classList.add('flick');
+        const properties = Object.keys(flick);
+        properties.forEach(property => {
+            const span = this.renderProperty(property, flick[property]);
+            !property.toLowerCase().includes('color') ? item.appendChild(span) : '';
+        });
+    
+        this.setColors(flick.backColor, flick.textColor, item);
+    
+        return item;
+    }
+    
+    
+    setColors(backColor, textColor, item) {
+        item.style.backgroundColor = backColor;
+        item.style.color = textColor;
+        item.style.border = `2px ${textColor} solid`;
+    }
 }
 
-const getItemProps = function(itemObj, form) {
-    itemObj.movie = form.movie.value;
-    itemObj.year = form.year.value;
-    itemObj.backColor = form.backColor.value;
-    itemObj.textColor = form.textColor.value;
-    itemObj.rating = form.rating.value;
-}
-
-const createItem = function(itemObj) {
-    const item = document.createElement('li');
-    const textItems = createText(itemObj.movie, itemObj.year, itemObj.rating);
-    textItems.forEach(textItem => item.appendChild(textItem));
-
-    setColors(itemObj.backColor, itemObj.textColor, item);
-
-    return item;
-}
-
-const createText = function(movie, year, rating) {
-    const movieItem = document.createElement('span');
-    const yearItem = document.createElement('span');
-    const ratingItem = document.createElement('span');
-
-    movieItem.textContent = `${movie} `;
-    yearItem.textContent = `(${year}) `;
-    ratingItem.textContent = rating;
-
-    movieItem.classList.add('movie');
-    yearItem.classList.add('year');
-    ratingItem.classList.add('rating');
-
-    return [movieItem, yearItem, ratingItem];
-}
-
-const setColors = function(backColor, textColor, item) {
-    item.style.backgroundColor = backColor;
-    item.style.color = textColor;
-    item.style.border = `2px ${textColor} solid`;
-}
-
-form.addEventListener('submit', addMovies);
+new App();
